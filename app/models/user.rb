e1cc2196eb72ce5febe4d8ad56f3b355e2b519fc
EC2 Default User <ec2-user@ip-172-31-30-99.ap-southeast-1.compute.internal>
2021-04-13 14:28:22 +0000

    _

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
+  def self.daily_email_sent
+    self.all.pluck(:email).each do |email|
+      DailyMailer.with(email: email).daily_email.deliver_later
+    end
+  end
 end
